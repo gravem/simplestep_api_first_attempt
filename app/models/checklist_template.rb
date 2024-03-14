74c1c866f1ebc4811d18de79669cbcbf159909c2
Aasmund Gravem <aasmund.gravem@gmail.com>
2024-03-14 16:18:03 +0000

    Fixed typos in checklist templates controller, added crud and authentificatin and relation to checklist templates in steps controller, added nsting of steps under checklist controller in routes.

--- app/models/checklist_template.rb
+++ app/models/checklist_template.rb
@@ -1,4 +1,7 @@
 class ChecklistTemplate < ApplicationRecord
   belongs_to :user
   has_many :steps, dependent: :destroy
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
