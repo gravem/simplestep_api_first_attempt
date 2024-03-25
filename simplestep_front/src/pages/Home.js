8ccf329467e44bd0885e6e931ac2924911ddc676
Aasmund Gravem <aasmund.gravem@gmail.com>
2024-03-25 22:59:05 +0000

    Seperating components, styling page and getting the login to work

--- simplestep_front/src/pages/Home.js
+++ simplestep_front/src/pages/Home.js
@@ -1,3 +1,13 @@
 export const Home = () => {
-  return <h1>Hello again</h1>;
+  return (
+    <div className='App'>
+      <h1 className=''>Hello again</h1>
+      <a
+        href='/login'
+        className='App-link'
+      >
+        Login
+      </a>
+    </div>
+  );
 };
